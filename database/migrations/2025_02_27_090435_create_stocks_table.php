5b3b5e50ce12afc1d9cc8112a88edd0071dc0efb
silvertouchvipul <silvertouchvipul@gmail.com>
2025-03-06 17:20:51 +0530

    stock migration add

--- database/migrations/2025_02_27_090435_create_stocks_table.php
+++ database/migrations/2025_02_27_090435_create_stocks_table.php
@@ -13,6 +13,19 @@ return new class extends Migration
     {
         Schema::create('stocks', function (Blueprint $table) {
             $table->id();
+            $table->string('location_id',50);
+            $table->string('location_name',50);
+            $table->string('edp_code',50);
+            $table->string('category',50);
+            $table->string('description',200);
+            $table->string('section',50);
+            $table->string('qty',50);
+            $table->string('measurement',50);
+            $table->string('new_spareable',50);
+            $table->string('used_spareable',50);
+            $table->string('remarks',200);
+            $table->integer('user_id')->nullable();
+          //  $table->integer('rig_id')->nullable();
             $table->timestamps();
         });
     }
