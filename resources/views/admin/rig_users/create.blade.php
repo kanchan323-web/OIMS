e3e50d7ead1357b460b3f7b85a751ff801a20366
abhinavjnambiar <abhinav.jnambiar@gmail.com>
2025-03-07 12:12:09 +0530

    rig user module creation

---
+++ resources/views/admin/rig_users/create.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.frontend.layout')
+@section('page-content')
+
+<div class="content-page">
+    <div class="container-fluid add-form-list">
+        <div class="row">
+            <div class="col-sm-12">
+                <div class="card">
+                    <div class="card-header d-flex justify-content-between">
+                        <div class="header-title">
+                            <h4 class="card-title">Add Rig User</h4>
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        <form action="{{ route('admin.rig_users.store') }}" method="POST" class="needs-validation" novalidate>
+                            @csrf
+                            <div class="form-row">
+                                <div class="col-md-6 mb-3">
+                                    <label for="name">Rig User Name</label>
+                                    <input type="text" class="form-control" name="name" required>
+                                </div>
+                            </div>
+                            <button class="btn btn-primary" type="submit">Submit</button>
+                            <a href="{{ route('admin.rig_users.index') }}" class="btn btn-light">Go Back</a>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
