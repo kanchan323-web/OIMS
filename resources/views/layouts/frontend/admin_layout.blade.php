949a9ff7e40f0bb097355c88be16491bdad51e0e
abhinavjnambiar <abhinav.jnambiar@gmail.com>
2025-03-07 12:56:51 +0530

    admin layout based changes

---
+++ resources/views/layouts/frontend/admin_layout.blade.php
@@ -0,0 +1,8 @@
+@include('layouts.frontend.header')
+@include('layouts.frontend.admin_sidebar')
+@include('layouts.frontend.navbar')
+
+                @yield('page-content')
+
+
+ @include('layouts.frontend.footer')
