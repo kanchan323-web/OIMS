4c3b0688b07b0dc46869e5e96501ee5897266bde
silvertouchvipul <silvertouchvipul@gmail.com>
2025-03-10 17:32:58 +0530

    working on category Crud

--- resources/views/admin/category/create.blade.php
+++ resources/views/admin/category/create.blade.php
@@ -5,6 +5,19 @@
     <div class="container-fluid add-form-list">
         <div class="row">
             <div class="col-sm-12">
+                
+                @if(session('error'))
+                <div class="alert alert-danger">
+                    {{ session('error') }}
+                </div>
+                @endif
+
+                @if(session('success'))
+                <div class="alert alert-success">
+                    {{ session('success') }}
+                </div>
+                @endif
+
                 <div class="card">
                     <div class="card-header d-flex justify-content-between">
                         <div class="header-title">
@@ -12,12 +25,13 @@
                         </div>
                     </div>
                     <div class="card-body">
-                        <form action="{{ route('admin.category.store') }}" method="POST" class="needs-validation" novalidate>
+                        <form action="{{ route('admin.category.store') }}" method="POST" class="needs-validation"
+                            novalidate>
                             @csrf
                             <div class="form-row">
                                 <div class="col-md-6 mb-3">
-                                    <label for="name">Rig Name</label>
-                                    <input type="text" class="form-control" name="name" required>
+                                    <label for="name">Category Name</label>
+                                    <input type="text" class="form-control" name="category_name" required>
                                 </div>
                             </div>
                             <button class="btn btn-primary" type="submit">Submit</button>
