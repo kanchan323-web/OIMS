6d6c6ef07cd6bed0c6952bd0a560e20ea4be048f
silvertouchvipul <silvertouchvipul@gmail.com>
2025-03-12 17:08:59 +0530

    Enhance Request Stock module: Display self & other requests, add request functionality

--- routes/web.php
+++ routes/web.php
@@ -103,6 +103,7 @@ Route::middleware(['auth'])->group(function () {
     //Request stock
     Route::prefix('request-stock')->group(function () {
         Route::get('/list', [RequestStockController::class, 'RequestStockList'])->name('request_stock_list');
+        Route::get('/generated', [RequestStockController::class, 'GeneratedRequest'])->name('request_generated_list');
         Route::get('/add', [RequestStockController::class, 'RequestStockAdd'])->name('request_stock_add');
         Route::post('/addSubmit', [RequestStockController::class, 'RequestStockAddPost'])->name('request_stock_add.post');
         Route::get('/view', [RequestStockController::class, 'RequestStockViewPost'])->name('request_stock_view.get');
