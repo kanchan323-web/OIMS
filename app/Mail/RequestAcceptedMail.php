9576f0c7e8f0f682e940e7553031fd97dbf06ab6
abhinavjnambiar <abhinav.jnambiar@gmail.com>
2025-03-20 16:20:16 +0530

    accept decline and query mail and after decline buttons hiding.

---
+++ app/Mail/RequestAcceptedMail.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class RequestAcceptedMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $mailData;
+
+    /**
+     * Create a new message instance.
+     */
+    public function __construct($mailData)
+    {
+        $this->mailData = $mailData;
+    }
+
+    /**
+     * Build the message.
+     */
+    public function build()
+    {
+        return $this->subject('Stock Request Accepted - ONGC')
+                    ->view('mails.request_accepted');
+    }
+}
---
+++ app/Mail/RequestQueryMail.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class RequestQueryMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $mailData;
+
+    /**
+     * Create a new message instance.
+     */
+    public function __construct($mailData)
+    {
+        $this->mailData = $mailData;
+    }
+
+    /**
+     * Build the message.
+     */
+    public function build()
+    {
+        return $this->subject('Stock Request Query Raised')
+                    ->view('mails.request_query')
+                    ->with('mailData', $this->mailData);
+    }
+}
