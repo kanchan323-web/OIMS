e5922a8b370e32b42d2bc5cb9df67c9028b97782
silvertouchvipul <silvertouchvipul@gmail.com>
2025-03-24 17:46:08 +0530

    Update migration: Make edp_code unique in edps table & add new columns to request_status.

---
+++ database/migrations/2025_03_24_120708_add_sent_to_and_sent_from_to_request_status_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('request_status', function (Blueprint $table) {
+            $table->string('sent_to', 100)->nullable()->after('rig_id');
+            $table->string('sent_from', 100)->nullable()->after('sent_to');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('request_status', function (Blueprint $table) {
+            //
+        });
+    }
+};
