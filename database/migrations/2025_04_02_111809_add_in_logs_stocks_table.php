d508a0237e8ea55e4845eb5c8572f8a8b7318f12
silvertouchvipul <silvertouchvipul@gmail.com>
2025-04-02 17:13:28 +0530

    complete log table implementation using migration

---
+++ database/migrations/2025_04_02_111809_add_in_logs_stocks_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('logs_stocks', function (Blueprint $table) {
+            $table->enum('req_status',['active','inactive'])->default('inactive')->after('rig_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('logs_stocks', function (Blueprint $table) {
+            $table->dropColumn('req_status');
+        });
+    }
+};
