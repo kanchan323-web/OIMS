75049aa02c14986bf20419b7e041ebce603cbbab
kanchan323-web <kanchansilvertouch@gmail.com>
2025-04-10 20:24:32 +0530

    fixs bugs on stock excel export

--- resources/views/pdf/report/stock/stock_report.blade.php
+++ resources/views/pdf/report/stock/stock_report.blade.php
@@ -10,7 +10,8 @@
             text-align: center;
         }
 
-        h1, h2 {
+        h1,
+        h2 {
             margin: 0;
             padding: 5px;
         }
@@ -21,10 +22,12 @@
             margin-top: 20px;
         }
 
-        th, td {
+        th,
+        td {
             border: 1px solid black;
             padding: 8px;
-            text-align: center; /* Center table content */
+            text-align: center;
+            /* Center table content */
         }
 
         th {
@@ -41,18 +44,16 @@
 </head>
 
 <body>
-@php $report_type = $data['report_type']; @endphp
+    @php $report_type = $data['report_type']; @endphp
 
     <h1>ONGC</h1>
-    @if($data['report_type']==1)
-
-
+    @if ($data['report_type'] == 1)
     @endif
 
 
     @switch($report_type)
-    @case('1')
-        <h2>Stock Overview Report</h2>
+        @case('1')
+            <h2>Stock Overview Report</h2>
             <table>
                 <thead>
                     <tr>
@@ -66,12 +67,12 @@
                     </tr>
                 </thead>
                 <tbody>
-                    @if($stockData->isEmpty())
+                    @if ($stockData->isEmpty())
                         <tr>
                             <td colspan="6" class="no-data">No data found</td>
                         </tr>
                     @else
-                        @foreach($stockData as $index => $stock)
+                        @foreach ($stockData as $index => $stock)
                             <tr>
                                 <td>{{ $loop->iteration }}</td>
                                 <td>{{ $stock->EDP_Code }}</td>
@@ -86,186 +87,194 @@
                 </tbody>
             </table>
         @break
-    @case('2')
-        <h2>Stock Additions Report</h2>
-        <table>
-            <thead>
-                <tr>
-                    <th>Sr.No</th>
-                    <th>EDP Code</th>
-                    <th>Description</th>
-                  <!--  <th>Total </th> -->
-                    <th>Add</th>
-                    <th>Rig Name</th>
-                    <th> Date</th>
-                </tr>
-            </thead>
-            <tbody>
-                @if($stockData->isEmpty())
-                    <tr>
-                        <td colspan="6" class="no-data">No data found</td>
-                    </tr>
-                @else
-                    @foreach($stockData as $index => $stock)
-                        <tr>
-                            <td>{{ $loop->iteration }}</td>
-                            <td>{{ $stock->EDP_Code }}</td>
-                            <td>{{ $stock->description }}</td>
-                      <!--      <td>{{ $stock->qty }}</td> -->
-                            <td>{{ $stock->requested_qty }}</td>
-                            <td>{{ $stock->name }}</td>
-                            <td>{{ $stock->created_at }}</td>
-                        </tr>
-                    @endforeach
-                @endif
-            </tbody>
-        </table>
-     @break
 
-    @case('3')
-        <h2>Stock Removals Report</h2>
-        <table>
-            <thead>
-                <tr>
-                    <th>Sr.No</th>
-                    <th>EDP Code</th>
-                    <th>Description</th>
-                   <!-- <th>Total </th> -->
-                    <th>Remove</th>
-                    <th>Rig Name</th>
-                    <th> Date</th>
-                </tr>
-            </thead>
-            <tbody>
-                @if($stockData->isEmpty())
+        @case('2')
+            <h2>Stock Additions Report</h2>
+            <table>
+                <thead>
                     <tr>
-                        <td colspan="6" class="no-data">No data found</td>
+                        <th>Sr.No</th>
+                        <th>EDP Code</th>
+                        <th>Description</th>
+                        <!--  <th>Total </th> -->
+                        <th>Add</th>
+                        <th>Supplier Rig</th>
+                        <th> Date</th>
                     </tr>
-                @else
-                    @foreach($stockData as $index => $stock)
+                </thead>
+                <tbody>
+                    @if ($stockData->isEmpty())
                         <tr>
-                            <td>{{ $loop->iteration }}</td>
-                            <td>{{ $stock->EDP_Code }}</td>
-                            <td>{{ $stock->description }}</td>
-                          <!--  <td>{{ $stock->qty }}</td> -->
-                            <td>{{ $stock->requested_qty }}</td>
-                            <td>{{ $stock->name }}</td>
-                            <td>{{ $stock->created_at }}</td>
+                            <td colspan="6" class="no-data">No data found</td>
                         </tr>
-                    @endforeach
-                @endif
-            </tbody>
-        </table>
-     @break
-     @case('4')
-     <h2>Stock Adjustments  Report</h2>
-         <table>
-             <thead>
-                 <tr>
-                    <th>Sr.No</th>
-                    <th>EDP Code</th>
-                    <th>Description</th>
-                    <th>Adjustments</th>
-                    <th>Rig Name</th>
-                    <th>Date</th>
-                 </tr>
-             </thead>
-             <tbody>
-                 @if($stockData->isEmpty())
-                     <tr>
-                         <td colspan="6" class="no-data">No data found</td>
-                     </tr>
-                 @else
-                     @foreach($stockData as $index => $stock)
-                         <tr>
-                            <td>{{ $loop->iteration }}</td>
-                            <td>{{ $stock->EDP_Code }}</td>
-                            <td>{{ $stock->description }}</td>
-                            <td>{{ $stock->qty }}</td>
-                            <td>{{ $stock->name }}</td>
-                            <td>{{ $stock->created_at }}</td>
-                         </tr>
-                     @endforeach
-                 @endif
-             </tbody>
-         </table>
-     @break
-    @case('5')
-        <h2>Stock Consumptions Report</h2>
-        <table>
-            <thead>
-                <tr>
-                    <th>Sr.No</th>
-                    <th>EDP Code</th>
-                    <th>Description</th>
-                    <th>Total </th>
-                    <th>Consumed</th>
-                    <th>Consumed Type</th>
-                    <th>Date</th>
-                </tr>
-            </thead>
-            <tbody>
-                @if($stockData->isEmpty())
-                    <tr>
-                        <td colspan="6" class="no-data">No data found</td>
-                    </tr>
-                @else
-                    @foreach($stockData as $index => $stock)
-                        <tr>
-                            <td>{{ $loop->iteration }}</td>
-                            <td>{{ $stock->EDP_Code }}</td>
-                            <td>{{ $stock->description }}</td>
-                            <td>{{ $stock->avl_qty }}</td>
-                            <td>{{ $stock->consume }}</td>
-                            <td>{{ $stock->name }}</td>
-                            <td>{{ $stock->created_at }}</td>
-                        </tr>
-                    @endforeach
-                @endif
-            </tbody>
-        </table>
-    @break
+                    @else
+                        @foreach ($stockData as $index => $stock)
+                            <tr>
+                                <td>{{ $loop->iteration }}</td>
+                                <td>{{ $stock->EDP_Code }}</td>
+                                <td>{{ $stock->description }}</td>
+                                <!--      <td>{{ $stock->qty }}</td> -->
+                                <td>{{ $stock->requested_qty }}</td>
+                                <td>{{ $stock->name }}</td>
+                                <td>{{ $stock->created_at }}</td>
+                            </tr>
+                        @endforeach
+                    @endif
+                </tbody>
+            </table>
+        @break
 
-    @case('6')
-        <h2>Stock Replenishment Report</h2>
-        <table>
-            <thead>
-                <tr>
-                    <th>Sr.No</th>
-                    <th>EDP Code</th>
-                    <th>Description</th>
-                  <!--  <th>Available </th> -->
-                    <th>Replenishment</th>
-                    <th>Status</th>
-                    <th>Date</th>
-                </tr>
-            </thead>
-            <tbody>
-                @if($stockData->isEmpty())
+        @case('3')
+            <h2>Stock Removals Report</h2>
+            <table>
+                <thead>
                     <tr>
-                        <td colspan="6" class="no-data">No data found</td>
+                        <th>Sr.No</th>
+                        <th>EDP Code</th>
+                        <th>Description</th>
+                        <!-- <th>Total </th> -->
+                        <th>Remove</th>
+                        <th>Rig Name</th>
+                        <th> Date</th>
                     </tr>
-                @else
-                    @foreach($stockData as $index => $stock)
+                </thead>
+                <tbody>
+                    @if ($stockData->isEmpty())
                         <tr>
-                            <td>{{ $loop->iteration }}</td>
-                            <td>{{ $stock->EDP_Code }}</td>
-                            <td>{{ $stock->description }}</td>
-                        <!--    <td>{{ $stock->avl_qty }}</td> -->
-                            <td>{{ $stock->replinish }}</td>
-                            <td>{{ $stock->name }}</td>
-                            <td>{{ $stock->created_at }}</td>
+                            <td colspan="6" class="no-data">No data found</td>
                         </tr>
-                    @endforeach
-                @endif
-            </tbody>
-        </table>
-    @break
+                    @else
+                        @foreach ($stockData as $index => $stock)
+                            <tr>
+                                <td>{{ $loop->iteration }}</td>
+                                <td>{{ $stock->EDP_Code }}</td>
+                                <td>{{ $stock->description }}</td>
+                                <!--  <td>{{ $stock->qty }}</td> -->
+                                <td>{{ $stock->requested_qty }}</td>
+                                <td>{{ $stock->name }}</td>
+                                <td>{{ $stock->created_at }}</td>
+                            </tr>
+                        @endforeach
+                    @endif
+                </tbody>
+            </table>
+        @break
 
-    @default
-         <table>
-            <thead><tr><td>No Data Found</td></tr></thead>
-        </table></p>
+        @case('4')
+            <h2>Stock Adjustments Report</h2>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Sr.No</th>
+                        <th>EDP Code</th>
+                        <th>Description</th>
+                        <th>Adjustments</th>
+                        <th>Rig Name</th>
+                        <th>Date</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @if ($stockData->isEmpty())
+                        <tr>
+                            <td colspan="6" class="no-data">No data found</td>
+                        </tr>
+                    @else
+                        @foreach ($stockData as $index => $stock)
+                            <tr>
+                                <td>{{ $loop->iteration }}</td>
+                                <td>{{ $stock->EDP_Code }}</td>
+                                <td>{{ $stock->description }}</td>
+                                <td>{{ $stock->qty }}</td>
+                                <td>{{ $stock->name }}</td>
+                                <td>{{ $stock->created_at }}</td>
+                            </tr>
+                        @endforeach
+                    @endif
+                </tbody>
+            </table>
+        @break
+
+        @case('5')
+            <h2>Stock Consumptions Report</h2>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Sr.No</th>
+                        <th>EDP Code</th>
+                        <th>Description</th>
+                        <th>Total </th>
+                        <th>Consumed</th>
+                        <th>Consumed Type</th>
+                        <th>Date</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @if ($stockData->isEmpty())
+                        <tr>
+                            <td colspan="6" class="no-data">No data found</td>
+                        </tr>
+                    @else
+                        @foreach ($stockData as $index => $stock)
+                            <tr>
+                                <td>{{ $loop->iteration }}</td>
+                                <td>{{ $stock->EDP_Code }}</td>
+                                <td>{{ $stock->description }}</td>
+                                <td>{{ $stock->avl_qty }}</td>
+                                <td>{{ $stock->consume }}</td>
+                                <td>{{ $stock->name }}</td>
+                                <td>{{ $stock->created_at }}</td>
+                            </tr>
+                        @endforeach
+                    @endif
+                </tbody>
+            </table>
+        @break
+
+        @case('6')
+            <h2>Stock Replenishment Report</h2>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Sr.No</th>
+                        <th>EDP Code</th>
+                        <th>Description</th>
+                        <!--  <th>Available </th> -->
+                        <th>Replenishment</th>
+                        <th>Status</th>
+                        <th>Date</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @if ($stockData->isEmpty())
+                        <tr>
+                            <td colspan="6" class="no-data">No data found</td>
+                        </tr>
+                    @else
+                        @foreach ($stockData as $index => $stock)
+                            <tr>
+                                <td>{{ $loop->iteration }}</td>
+                                <td>{{ $stock->EDP_Code }}</td>
+                                <td>{{ $stock->description }}</td>
+                                <!--    <td>{{ $stock->avl_qty }}</td> -->
+                                <td>{{ $stock->replinish }}</td>
+                                <td>{{ $stock->name }}</td>
+                                <td>{{ $stock->created_at }}</td>
+                            </tr>
+                        @endforeach
+                    @endif
+                </tbody>
+            </table>
+        @break
+
+        @default
+            <table>
+                <thead>
+                    <tr>
+                        <td>No Data Found</td>
+                    </tr>
+                </thead>
+            </table>
+            </p>
     @endswitch
 
 </body>
