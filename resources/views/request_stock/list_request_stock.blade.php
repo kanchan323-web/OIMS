0a321eb33418a41fae506067828861f0d5a9cf08
silvertouchvipul <silvertouchvipul@gmail.com>
2025-04-23 11:20:40 +0530

    apply  breadcrumbs on user side

--- resources/views/request_stock/list_request_stock.blade.php
+++ resources/views/request_stock/list_request_stock.blade.php
@@ -4,6 +4,7 @@
         <div class="container-fluid">
             <div class="row">
                 <div class="col-lg-12">
+                    {{Breadcrumbs::render('incoming_request_stock_list')}}
                     @if (Session::get('success'))
                         <div class="alert bg-success text-white alert-dismissible fade show" role="alert">
                             <strong>Success:</strong> {{ Session::get('success') }}
