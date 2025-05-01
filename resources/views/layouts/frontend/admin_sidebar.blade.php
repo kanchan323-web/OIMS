067ebd905050745e2f04979edf152f4fdc427924
kanchan323-web <kanchansilvertouch@gmail.com>
2025-05-01 11:46:55 +0530

    change  Rename it to "Requests List"

--- resources/views/layouts/frontend/admin_sidebar.blade.php
+++ resources/views/layouts/frontend/admin_sidebar.blade.php
@@ -96,7 +96,7 @@
                             <rect x="9" y="9" width="13" height="13" rx="2" ry="2"></rect>
                             <path d="M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1"></path>
                         </svg>
-                        <span class="ml-4">Request Stock List</span>
+                        <span class="ml-4">Requests List</span>
                     </a>
                 </li>
 
